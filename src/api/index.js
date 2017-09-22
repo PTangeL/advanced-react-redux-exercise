1a3c9cfca667f473c7e060b3a464652e188eb049
alexlbr <alex.lobera@gmail.com>
2017-09-22 15:21:53 +0100

    fixed fetch

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 import linkHeader from 'parse-link-header'
+import 'whatwg-fetch'
 
 import { API_SEARCH_USER_BASE_URL } from '../config'
 
@@ -6,7 +7,7 @@ export const fetchUsers = ({ nextUrl, query }) => {
   const url = nextUrl || `${API_SEARCH_USER_BASE_URL}?q=${query}&sort=followers`
 
   return fetch(url).then( response => {
-    const link = linkHeader(response.headers.getAll('Link')[0])
+    const link = linkHeader(response.headers.get("Link"))
     return response.json().then( users => ({
       nextUrl: link.next.url,
       users
